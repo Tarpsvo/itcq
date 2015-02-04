f941cc54f13c8ead8410efde5d74ab85584bac28
T.Tramvai <Tarvo@tramvai.im>
2015-02-04 21:59:10 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>The IT College Quiz</title>
+
+        <link rel="stylesheet" href="css/cssreset.css"> <!-- Eric Meyer's Reset CSS v2.0 - http://cssreset.com -->
+        <link rel="stylesheet" href="css/main.css">
+
+    </head>
+
+    <body>
+        <p>TODO content</p>
+    </body>
+</html>
