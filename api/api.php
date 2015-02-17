2a556634814dcd773721b40a1ad019c94e10206a
Tarvo R <tarvo@tramvai.im>
2015-02-17 23:00:48 +0200

    Question add interface and question list.

--- api/api.php
+++ api/api.php
@@ -1,9 +1,19 @@
 <?php
 // Allowed queries:
-// ql = question list (from database)
+// ql = question list for admin view
+// cat = category list for new question view
 
-$allowedQueries = ['ql'];
-$queryType = $_GET['request'];
+if (isset($_GET['request'])) $queryType = $_GET['request']; else {
+    $postedData = file_get_contents("php://input");
+    $data = json_decode($postedData);
+    $queryType = $data->request;
+}
+
+$allowedQueries = ['ql', 'cat', 'add'];
+$allowedQueriesList = [
+    'ql'    => "SELECT id, category, question, answer, enabled FROM questions",
+    'cat'   => "SELECT name FROM categories",
+];
 
 // If request is not an allowed one, exit program.
 if (!in_array($queryType, $allowedQueries)) die("Query not allowed!");
@@ -12,13 +22,18 @@ if (!in_array($queryType, $allowedQueries)) die("Query not allowed!");
 $connection = connectToDatabase();
 
 switch ($queryType) {
-    case 'ql':
-        getQuestionList($connection);
+    case ($queryType == 'ql' || $queryType == 'cat'):
+        queryAndMakeJson($connection, $allowedQueriesList[$queryType]);
+    break;
+
+    case 'add':
+        $validatedData = validateData($data);
+        addNewQuestion($connection, $validatedData);
     break;
 
     default:
         # code...
-    break;
+        break;
 }
 
 function connectToDatabase() {
@@ -35,22 +50,45 @@ function connectToDatabase() {
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connection;
     } catch (PDOException $e) {
-        die("ERROR: Connection failed: " . $e->getMessage());
+        die(createErrorJson($e->getMessage()));
     }
 }
 
-function getQuestionList($connection) {
-        $sql = 'SELECT * FROM questions';
-        $queryResult = $connection->query($sql);
+function queryAndMakeJson($connection, $sql) {
+    $queryResult = $connection->query($sql);
 
-        // Learned from http://www.w3schools.com/php/php_mysql_select.asp
-        if ($queryResult) {
-            // JSON part learned from http://www.phpro.org/tutorials/Consume-Json-Results-From-PHP-MySQL-API-With-Angularjs-And-PDO.html
-            $queryResult = $queryResult->fetchAll( PDO::FETCH_ASSOC );
-            $json = json_encode($queryResult);
-            echo $json;
-        }
+    // Learned from http://www.w3schools.com/php/php_mysql_select.asp
+    if ($queryResult) {
+        // JSON part learned from http://www.phpro.org/tutorials/Consume-Json-Results-From-PHP-MySQL-API-With-Angularjs-And-PDO.html
+        $queryResult = $queryResult->fetchAll( PDO::FETCH_ASSOC );
+        $json = json_encode($queryResult);
+        echo $json;
+    } else {
+        createErrorJson("Query didn't return any results.");
+    }
 }
 
+function createErrorJson($error) {
+    $json = json_encode(array('ERROR' => $error));
+    echo $json;
+}
+
+function validateData($data) {
+    $q['question'] = $data->question;
+    $q['category'] = $data->category;
+    $q['answer'] = $data->answer;
+    $q['wrong'] = $data->wrong;
+    if ($data->enabled == 'true') $q['enabled'] = 1; else $q['enabled'] = 0;
+
+    if (!$q['question'] || !$q['category'] || !$q['answer'] || !$q['wrong'] || !$q['enabled']) die("One was empty");
+
+    return $q;
+}
+
+function addNewQuestion($connection, $q) {
+    $sql = "INSERT INTO questions (category, question, answer, wrongs, enabled) VALUES ('".$q['category']."', '".$q['question']."', '".$q['answer']."', '".$q['wrong']."', '".$q['enabled']."')";
+
+    $connection->exec($sql);
+    }
 
 ?>
--- js/itcqAdmin.js
+++ js/itcqAdmin.js
@@ -35,4 +35,38 @@ itcqAdmin.controller('itcqAdminCtrl', function ($scope, $location, $http) {
                 $scope.questionList = data;
             });
     };
+
+    $scope.getCategories = function() {
+        console.log("itcqAdminCtrl: getCategories quering categories.");
+
+        $http.get('http://localhost/api/api.php?request=cat')
+            .success(function(data) {
+                console.log("itcqAdminCtrl: getCategories was successful. Assigning to scope.");
+                $scope.categoriesList = data;
+            });
+    };
+});
+
+// Learned from https://docs.angularjs.org/api/ng/directive/input
+itcqAdmin.controller('questionFormCtrl', function ($scope, $location, $http) {
+    $scope.addNewQuestion = function() {
+        if ($scope.q) {
+            console.log("itcqAdmin: questionFieldCtrl: Question data received. Passing onto API.");
+            $scope.passDataToAPI($scope.q);
+            console.log($scope.q);
+        }
+    };
+
+    $scope.passDataToAPI = function($info) {
+        console.log("Posting the following: ");
+        console.log('request: add, question:'+ $info.question+ ',category:'+ $info.category+ ',answer:'+ $info.answer+ ',wrong:'+ $info.wrong+ ',enabled:' +$info.enabled);
+        $http.post('../api/api.php', {'request': 'add', 'question': $info.question, 'category': $info.category, 'answer': $info.answer, 'wrong': $info.wrong, 'enabled': $info.enabled})
+            .success(function() {
+                console.log("itcqAdmin: questionFieldCtrl: question data successfully passed to API.");
+            })
+            .error(function(data, status) { // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            $scope.errors.push(status);
+            });
+    }
 });
